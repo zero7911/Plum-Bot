bd03d9ab614ab0cd00d034d0989c5838049fea2b
Zero <zero07911@gmail.com>
2022-04-14 21:32:02 +0500

    updated

--- cogs/guides.py
+++ cogs/guides.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 class Guides(commands.Cog, name='Guides'):
     @commands.command(name="guide")
     async def _guides_(self, ctx, message=None):
-        with open(f"guides\{message}.txt", 'r') as f:
+        with open(f"{message}.txt", 'r') as f:
             guide = f.read(2000)
             if len(guide) >= 2000:
                 x = guide.rfind('\n')
